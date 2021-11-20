f17e1a1b367f486e19d417371711b3acb576f033
Gonzalo Vila <gonvila84@gmail.com>
2021-11-20 14:27:26 +0100

    Se agregan los datos del alumno VILA, Gonzalo

--- src/alumnos.c
+++ src/alumnos.c
@@ -31,8 +31,15 @@ static const struct alumno_s ESTEBAN_VOLENTINI = {
     .documento = "23.517.968",
 };
 
+static const struct alumno_s GONZALO_VILA = {
+    .apellidos = "VILA",
+    .nombres = "Gonzalo Vila",
+    .documento = "31.061.45",
+};
+
 const alumno_t ALUMNOS[] = {
     &ESTEBAN_VOLENTINI,
+    &GONZALO_VILA,
 };
 
 const int CANTIDAD_ALUMNOS = (sizeof(ALUMNOS) / sizeof(alumno_t));
